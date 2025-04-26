dcba1683be9e275b84d9b9c1276e4e8e4d9c7695
sauron <kravshanov97@gmail.com>
2025-04-26 16:12:40 +0500

    created IsAuthenticatedCustom Permission and requested auth project everything going well no problem. but some changes need it to docker-compose.yml file

--- product_service/products/permissions.py
+++ product_service/products/permissions.py
@@ -5,59 +5,28 @@ from rest_framework.response import Response
 import requests
 from rest_framework import status
 
-AUTH_SERVICE_URL = "http://localhost:8000/accounts/api/token/verify/"
+AUTH_SERVICE_URL = "http://auth-service:8000/accounts/api/token/verify/"
 
 
 class IsAuthenticatedCustom(BasePermission):
 
     def has_permission(self, request, view):
         token = request.headers.get("Authorization", "").replace('Bearer ', '')
+
         if not token:
-            raise AuthenticationFailed("Token required")
+            raise AuthenticationFailed("Token topilmadi")
 
-        try:
-            if token is None:
-                return Response({"detail": "Token topilmadi"}, status=status.HTTP_401_UNAUTHORIZED)
-            response = requests.post(AUTH_SERVICE_URL, data={"token": token})
+        response = requests.post(AUTH_SERVICE_URL, data={'token': token})
 
-            if response.status_code != 200:
-                return Response({"detail": "Token noto'g'ri yoki foydalanuvchi mavjud emas"},
-                                status=status.HTTP_401_UNAUTHORIZED)
-            return token
-        except Exception as e:
-            return Response({"error": str(e)})
+        if response.status_code != 200:
+            raise AuthenticationFailed("Token incorrect or user doesn't exist.!")
 
+        user_id = decode_token(token)
+        if not user_id:
+            raise AuthenticationFailed('user not found.!')
 
+        request.user_id = user_id
 
+        return True
 
-#
-
-
-# @api_view(['POST'])
-# # @permission_classes([IsAuthenticated])
-# def create_product(request):
-#     token = request.headers.get("Authorization", "").replace('Bearer ', '')
-#
-#     if token is None:
-#         return Response({"detail": "Token topilmadi"}, status=status.HTTP_401_UNAUTHORIZED)
-#
-#     response = requests.post(AUTH_SERVICE_URL, data={"token": token})
-#
-#     if response.status_code != 200:
-#         return Response({"detail": "Token noto'g'ri yoki foydalanuvchi mavjud emas"},
-#                         status=status.HTTP_401_UNAUTHORIZED)
-#
-#     user_id = decode_token(token)
-#     if not user_id:
-#         return Response({"detail": "Foydalanuvchi aniqlanmadi"}, status=status.HTTP_401_UNAUTHORIZED)
-#
-#     data = request.data.copy()
-#     data['user_id'] = user_id
-#
-#     serializer = ProductSerializer(data=request.data)
-#     if serializer.is_valid():
-#         serializer.save(user_id=user_id)
-#
-#         return Response(serializer.data, status=status.HTTP_201_CREATED)
-#     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
--- product_service/products/views.py
+++ product_service/products/views.py
@@ -1,5 +1,4 @@
-import jwt
-from django.conf import settings
+
 import requests
 from config.utils import decode_token
 from rest_framework.views import APIView
@@ -10,46 +9,15 @@ from .serializers import CategorySerializer, ProductListSerializer, ProductSeria
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from .models import Product, Category
 from .permissions import IsAuthenticatedCustom
-from django.http import HttpRequest
-from rest_framework.authentication import TokenAuthentication
-
-AUTH_SERVICE_URL = "http://localhost:8000/accounts/api/token/verify/"
-
-
-# def decode_token(token):
-#     try:
-#         payload = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
-#         return payload.get("user_id")
-#     except jwt.ExpiredSignatureError:
-#         return None
-#     except jwt.InvalidTokenError:
-#         return None
 
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticatedCustom])
 def create_product(request):
-    token = request.headers.get("Authorization", "").replace('Bearer ', '')
-
-    if token is None:
-        return Response({"detail": "Token topilmadi"}, status=status.HTTP_401_UNAUTHORIZED)
-
-    response = requests.post(AUTH_SERVICE_URL, data={"token": token})
-
-    if response.status_code != 200:
-        return Response({"detail": "Token noto'g'ri yoki foydalanuvchi mavjud emas"},
-                        status=status.HTTP_401_UNAUTHORIZED)
-
-    user_id = decode_token(token)
-    if not user_id:
-        return Response({"detail": "Foydalanuvchi aniqlanmadi"}, status=status.HTTP_401_UNAUTHORIZED)
-
-    data = request.data.copy()
-    data['user_id'] = user_id
 
     serializer = ProductSerializer(data=request.data)
     if serializer.is_valid():
-        serializer.save(user_id=user_id)
+        serializer.save(user_id=request.user_id)
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -69,7 +37,7 @@ class ProductListApiView(APIView):
 
 
 class CreateCategoryApiView(APIView):
-    # permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedCustom]
 
     def post(self, request):
         serializer = CategorySerializer(data=request.data)
