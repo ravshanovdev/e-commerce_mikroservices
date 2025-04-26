bddd0c21990e93ba67af58b2c5ec8052b04f46ca
Kamoliddin <108530207+ravshanovdev@users.noreply.github.com>
2025-04-27 03:41:59 +0500

    Delete payment/payment_modul directory

--- payment/payment_modul/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import CreateSessionPaymentApiView
-
-
-urlpatterns = [
-    path('create_payment/<int:product_id>/', CreateSessionPaymentApiView.as_view(), ),
-
-]
-
